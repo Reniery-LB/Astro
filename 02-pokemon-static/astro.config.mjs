9e93c6db096b01cf3655cfc923a274f42d540f8c
Reniery-lb <relu_23@alu.uabcs.mx>
2026-09-05 16:02:23 -0700

    S4-pokemon-L06

--- 02-pokemon-static/astro.config.mjs
+++ 02-pokemon-static/astro.config.mjs
@@ -1,14 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        ignored: [
+          '**/*.tmp',
+          '**/DumpStack.log.tmp',
+          'C:/DumpStack.log.tmp',
+          'C:\\DumpStack.log.tmp',
+          'C:/pagefile.sys',
+          'C:/swapfile.sys',
+          'C:/hiberfil.sys'
+        ]
+      }
+    }
   },
 
   integrations: [icon()]
