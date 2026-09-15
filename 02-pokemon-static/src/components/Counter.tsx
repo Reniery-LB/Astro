b3d3773d8d4830c1fee344b1fa1da85dac5d8f47
Reniery-lb <relu_23@alu.uabcs.mx>
2026-09-14 22:27:46 -0700

    S4-pokemon-L11

--- 02-pokemon-static/src/components/Counter.tsx
+++ 02-pokemon-static/src/components/Counter.tsx
@@ -1,19 +1,25 @@
-import { createSignal } from "solid-js"
+import { createSignal, type Component, type JSX } from "solid-js"
 
+interface Props {
+    initValue?: number;
+    children?: JSX.Element;
+}
 
-export const Counter = () => {
+export const Counter: Component<Props> = (props) => {
 
-    const [counter, setCounter] = createSignal(10)
+    const [counter, setCounter] = createSignal(props.initValue ?? 0);
 
     return (
         <>
-            <h1 class="text-5xl mb-5">Counter</h1>
+            {/* <h1 class="text-5xl mb-5">Counter</h1> */}
+            {props.children}
+
             <h3 class="text-8xl mb-5">Value: {counter()}</h3>
 
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
-                onClick={() => setCounter((prev) => ++prev)}>+1</button>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
                 onClick={() => setCounter((prev) => --prev)}>-1</button>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+                onClick={() => setCounter((prev) => ++prev)}>+1</button>
         </>
     )
 }
