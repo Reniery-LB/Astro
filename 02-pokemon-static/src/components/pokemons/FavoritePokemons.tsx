a451676680d5eb440efacf5bd28c7a27363b399a
Reniery-lb <relu_23@alu.uabcs.mx>
2026-09-15 21:25:21 -0700

    S4-pokemon-L12

---
+++ 02-pokemon-static/src/components/pokemons/FavoritePokemons.tsx
@@ -0,0 +1,23 @@
+import type { FavoritePokemon } from "@interfaces/favorite-pokemons";
+import { For, createSignal } from "solid-js";
+
+const getLocalStoragePokemons = (): FavoritePokemon[] => {
+    const favoritePokemons = JSON.parse(
+        localStorage.getItem('favorites') ?? '[]'
+    )
+
+    return favoritePokemons;
+}
+
+export const FavoritePokemons = () => {
+
+    const [pokemons, setPokemons] = createSignal(getLocalStoragePokemons());
+
+    return <div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
+        <For each={pokemons()}>
+            {
+                (pokemon) => <h1>{pokemon.name}</h1>
+            }
+        </For>
+    </div>
+};
